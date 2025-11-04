2d57c6f638644f5edabb5763d42b7d61974d6609
orokidd <gregomily@gmail.com>
2025-11-04 14:19:25 +0800

    express validator on delete genre

--- controllers/genre.js
+++ controllers/genre.js
@@ -1,42 +1,60 @@
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
 
 async function genreGet(req, res) {
-    const genres = await db.getAllGenres();
-    res.render("genres", { genres })
+  const genres = await db.getAllGenres();
+  res.render("genres", { genres });
 }
 
 async function newGenreGet(req, res) {
-    res.render("newGenre")
+  res.render("newGenre");
 }
 
 async function newGenrePost(req, res) {
-    const { genre_name } = req.body
-    await db.addNewGenre(genre_name);
-    res.redirect("/genres")
+  const { genre_name } = req.body;
+  await db.addNewGenre(genre_name);
+  res.redirect("/genres");
 }
 
 async function deleteGenreGet(req, res) {
-    const genreId = req.params.genreId;
-    const genreName =  await db.getGenreNameById(genreId);
-    res.render("deleteGenre", { genreName, genreId });
+  const genreId = req.params.genreId;
+  const genreName = await db.getGenreNameById(genreId);
+  res.render("deleteGenre", { genreName, genreId });
 }
 
-async function deleteGenrePost(req, res) {
-    const genreId = req.params.genreId;
-    const adminPassword = req.body.admin_password;
+const deleteGenrePost = [
+  body("admin_password")
+    .trim()
+    .notEmpty().withMessage("Password is required.")
+    .custom((value) => {
+      if (value !== process.env.ADMIN_PASSWORD) {
+        throw new Error("Wrong password. Please try again.");
+      }
+      return true;
+    }),
 
-    // Simple password check (in a real app, use proper authentication)
-    if (adminPassword !== process.env.ADMIN_PASSWORD) {
-        return res.status(403).send("Forbidden: Incorrect admin password");
+  async (req, res) => {
+    const genreId = req.params.genreId;
+    const genreName = await db.getGenreNameById(genreId);
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.render("deleteGenre", {
+        genreId,
+        genreName,
+        errors: errors.array(),
+      });
     }
+
     await db.deleteGenreById(genreId);
     res.redirect("/genres");
-}
+  },
+];
 
 module.exports = {
-    genreGet,
-    newGenreGet,
-    newGenrePost,
-    deleteGenrePost,
-    deleteGenreGet
-}
+  genreGet,
+  newGenreGet,
+  newGenrePost,
+  deleteGenrePost,
+  deleteGenreGet,
+};
