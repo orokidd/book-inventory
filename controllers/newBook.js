85b0e86e0bfad602f3d92b291219fd63984abdd4
orokidd <gregomily@gmail.com>
2025-11-09 14:42:52 +0800

    new book form style

--- controllers/newBook.js
+++ controllers/newBook.js
@@ -3,7 +3,7 @@ const { body, validationResult } = require("express-validator");
 
 async function newBookGet(req, res) {
     const genres = await db.getAllGenres();
-    res.render("newBook", { genres, previous: {} })
+    res.render("./newBook/newBook", { genres, previous: {} })
 }
 
 const addBook = [
@@ -51,7 +51,7 @@ const addBook = [
 
         if (!errors.isEmpty()) {
             const allGenres = await db.getAllGenres();
-            return res.render("newBook", { genres: allGenres, previous: req.body, errors: errors.array() });
+            return res.render("./newBook/newBook", { genres: allGenres, previous: req.body, errors: errors.array() });
         }
 
         try {
