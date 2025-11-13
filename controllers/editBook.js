325293be943bba66db8ef8037d3781efb8c05ba3
orokidd <gregomily@gmail.com>
2025-11-13 16:50:22 +0800

    fix a bug on cancel edit book

--- controllers/editBook.js
+++ controllers/editBook.js
@@ -12,76 +12,62 @@ async function editBookGet(req, res) {
 }
 
 const editBookPost = [
-    body("title")
-        .trim()
-        .notEmpty().withMessage("Title is required.")
-        .isLength({ max: 255 }).withMessage("Title must be at most 255 characters long."),
-    body("author")
-        .trim()
-        .notEmpty().withMessage("Author is required.")
-        .isLength({ max: 255 }).withMessage("Author must be at most 255 characters long."),
-    body("published_year")
-        .optional({ checkFalsy: true })
-        .isInt({ min: 0 }).withMessage("Published year must be a valid year."),
-    body("isbn")
-        .trim()
-        .optional({ checkFalsy: true })
-        .isLength({ max: 20 }).withMessage("ISBN must be at most 20 characters long."),
-    body("description")
-        .trim()
-        .optional({ checkFalsy: true }),
-    body("pages")
-        .optional({ checkFalsy: true })
-        .isInt({ min: 1 }).withMessage("Pages must be a positive number."),
-    body("stock")
-        .optional({ checkFalsy: true })
-        .isInt({ min: 0 }).withMessage("Stock must be a non-negative number."),
-    body("image_url")
-        .trim()
-        .optional({ checkFalsy: true })
-        .isURL().withMessage("Image URL must be a valid URL."),
-    body("admin_password")
-        .trim()
-        .notEmpty().withMessage("Admin password is required.")
-        .custom((value) => {
-            if (value !== process.env.ADMIN_PASSWORD) {
-                throw new Error("Wrong password. Please try again.");
-            }
-            return true;
-        }),
+  body("title").trim().notEmpty().withMessage("Title is required.").isLength({ max: 255 }).withMessage("Title must be at most 255 characters long."),
+  body("author").trim().notEmpty().withMessage("Author is required.").isLength({ max: 255 }).withMessage("Author must be at most 255 characters long."),
+  body("published_year").optional({ checkFalsy: true }).isInt({ min: 0 }).withMessage("Published year must be a valid year."),
+  body("isbn").trim().optional({ checkFalsy: true }).isLength({ max: 20 }).withMessage("ISBN must be at most 20 characters long."),
+  body("description").trim().optional({ checkFalsy: true }),
+  body("pages").optional({ checkFalsy: true }).isInt({ min: 1 }).withMessage("Pages must be a positive number."),
+  body("stock").optional({ checkFalsy: true }).isInt({ min: 0 }).withMessage("Stock must be a non-negative number."),
+  body("image_url").trim().optional({ checkFalsy: true }).isURL().withMessage("Image URL must be a valid URL."),
+  body("admin_password")
+    .trim()
+    .notEmpty()
+    .withMessage("Admin password is required.")
+    .custom((value) => {
+      if (value !== process.env.ADMIN_PASSWORD) {
+        throw new Error("Wrong password. Please try again.");
+      }
+      return true;
+    }),
 
-    async (req, res) => {
-        const { title, author, published_year, isbn, description, pages, stock, image_url, genres } = req.body;
-        const bookId = req.params.bookId;
-        const errors = validationResult(req);
+  async (req, res) => {
+    const { title, author, published_year, isbn, description, pages, stock, image_url, genres } = req.body;
+    const bookId = req.params.bookId;
+    const errors = validationResult(req);
+    const sourcePage = req.query.from;
 
-        if (!errors.isEmpty()) {
-            const allGenres = await db.getAllGenres();
-            return res.render("./edit-book/edit-book", { book: req.body, genres: allGenres, errors: errors.array() });
-        }
-
-        try {
-            await db.updateBook(bookId, {
-                title,
-                author,
-                published_year,
-                isbn,
-                description,
-                pages,
-                stock,
-                image_url,
-            });
-
-            if (genres) {
-                await db.addGenresToBookByNames(bookId, genres);
-            }
-
-            res.redirect("/");
-        } catch (error) {
-            console.error("Error adding book:", error);
-            res.status(500).send("Error adding book");
-        }
+    if (!errors.isEmpty()) {
+      const allGenres = await db.getAllGenres();
+      return res.render("./edit-book/edit-book", { book: {...req.body, id: bookId}, genres: allGenres, errors: errors.array(), sourcePage });
     }
+
+    try {
+      await db.updateBook(bookId, {
+        title,
+        author,
+        published_year,
+        isbn,
+        description,
+        pages,
+        stock,
+        image_url,
+      });
+
+      if (genres) {
+        await db.addGenresToBookByNames(bookId, genres);
+      }
+
+      if (sourcePage === "details") {
+        res.redirect(`/${bookId}`);
+      } else {
+        res.redirect("/");
+      }
+    } catch (error) {
+      console.error("Error adding book:", error);
+      res.status(500).send("Error adding book");
+    }
+  },
 ];
 
 module.exports = {
