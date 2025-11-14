a09c3b262d770c78154553917dc15e5333a26a1e
orokidd <gregomily@gmail.com>
2025-11-14 14:49:58 +0800

    change pool from local database to hosted database

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,8 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
